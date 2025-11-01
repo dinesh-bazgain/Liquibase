a3fd55a4a4007d27105afcbbada405a07c9a7dc7
dinesh-bazgain <dinesh.bazgain@gmail.com>
2025-11-01 08:11:12 +0545

    Add TourModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LiquibaseModule } from './liquibase/liquibase.module';
+import { TourModule } from './liquibase/tour/tour.module';
 
 @Module({
-  imports: [LiquibaseModule],
+  imports: [LiquibaseModule, TourModule],
   controllers: [AppController],
   providers: [AppService],
 })
